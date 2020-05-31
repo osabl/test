ff70739177869cf8b199983c5abfcc25333b895d
osabl <andrii.osabliuk@gmail.com>
2020-05-31 21:50:58 +0300

    add component

---
+++ src/components/Recommendation.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Would you recommend our service to others?</h2>
+    <Rating :rating="rating" @update-rating="$emit('update-rating', $event)"/>
+  </div>
+</template>
+
+<script>
+import Rating from '@/components/Rating.vue'
+
+export default {
+  components: {
+    Rating
+  },
+  props: {
+    rating: Number
+  }
+}
+</script>
