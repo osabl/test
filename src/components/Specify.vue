12d4b73597868ab5289680114aef1224e5c96c6a
osabl <andrii.osabliuk@gmail.com>
2020-05-31 22:32:45 +0300

    add component

---
+++ src/components/Specify.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Please specify</h2>
+    <Multiselect :selection="selection" :selected="specify" @update-selected="$emit('update-selected', $event)"/>
+  </div>
+</template>
+
+<script>
+import Multiselect from '@/components/Multiselect.vue'
+
+export default {
+  components: {
+    Multiselect
+  },
+  props: {
+    specify: Array
+  },
+  data () {
+    return {
+      selection: [
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven'
+      ]
+    }
+  }
+}
+</script>
